a250ec837fc3b4050eda70c9474144d24a110179
Patrik Cavrić <80886976+LoStGhost8-PatrikCavric@users.noreply.github.com>
2022-05-02 21:57:31 +0200

    continue

--- 3ProjektniZadatak/Assets/Scripts/Characters/Player.cs
+++ 3ProjektniZadatak/Assets/Scripts/Characters/Player.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
+    public float speed = 0f;
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();    
     }
 
     // Update is called once per frame
@@ -16,39 +17,48 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0, speed, 0);
+            transform.Translate(0, speed * Time.deltaTime, 0);
+            anim.SetInteger("AnimState", 1);
             if (Input.GetKey(KeyCode.A))
             {
-                transform.Translate(-speed, 0, 0);
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
             }
             else if (Input.GetKey(KeyCode.D))
             {
-                transform.Translate(speed, 0, 0);
+                transform.Translate(speed * Time.deltaTime, 0, 0);
             }
 
         }
 
         else if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(0, -speed, 0);
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+            anim.SetInteger("AnimState", 2);
             if (Input.GetKey(KeyCode.A))
             {
-                transform.Translate(-speed, 0, 0);
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
             }
             else if (Input.GetKey(KeyCode.D))
             {
-                transform.Translate(speed, 0, 0);
+                transform.Translate(speed * Time.deltaTime, 0, 0);
             }
         }
 
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-speed, 0, 0);
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+            anim.SetInteger("AnimState", 3);
         }
 
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(speed, 0, 0);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+            anim.SetInteger("AnimState", 4);
+        }
+
+        else
+        {
+            anim.SetInteger("AnimState", 0);
         }
     }
 }
