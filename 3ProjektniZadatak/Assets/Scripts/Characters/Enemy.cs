21a6e77cb7b3658b444eb7aecf69eb8e0f7bb442
Patrik Cavrić <80886976+LoStGhost8-PatrikCavric@users.noreply.github.com>
2022-05-06 15:58:35 +0200

    Enemy

---
+++ 3ProjektniZadatak/Assets/Scripts/Characters/Enemy.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject bullet;
+    public Color bulletColor;
+
+    public float xSpeed;
+    public float ySpeed;
+
+    public bool canShoot;
+    public float fireRate;
+    public int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!canShoot)
+        {
+            return;
+        }
+        fireRate = fireRate + (Random.Range(fireRate / -2, fireRate / 2));
+        InvokeRepeating("Shoot", fireRate, fireRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Spaceship>().TakeDamage();
+            
+        }
+    }
+
+    public void Death()
+    {
+        Destroy(gameObject);
+    }
+
+    public void TakeDamage()
+    {
+        health--;
+        if (health == 0)
+        {
+            Death();
+        }
+    }
+    public void Shoot()
+    {
+        GameObject temp = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+        temp.GetComponent<Bullet>().ChangeDirection();
+        temp.GetComponent<Bullet>().ChangeColor(bulletColor);
+    }
+}
