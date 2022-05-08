2ff232fbfefd41e9a7b622d086649e199bbdf611
Patrik Cavrić <80886976+LoStGhost8-PatrikCavric@users.noreply.github.com>
2022-05-08 19:11:19 +0200

    NPC

--- 3ProjektniZadatak/Assets/Scripts/SceneChange1.cs
+++ 3ProjektniZadatak/Assets/Scripts/SceneChange1.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChange : MonoBehaviour
+public class SceneChange1 : MonoBehaviour
 {
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         }
     }
     // Start is called before the first frame update
